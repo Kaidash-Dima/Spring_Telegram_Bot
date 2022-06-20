f22291cbe10440149f46f19ee92bd8f35ea4050d
Dmitriy kaidash <dmitriykaidash1@gmail.com>
2022-06-20 16:54:12 +0200

    e

--- src/main/java/com/kaidash/match/controller/WebHookController.java
+++ src/main/java/com/kaidash/match/controller/WebHookController.java
@@ -16,8 +16,13 @@ public class WebHookController {
         this.bot = bot;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update){
-        return bot.onWebhookUpdateReceived(update);
+    @GetMapping("/")
+    public String ok(){
+        return "hello!!!";
     }
+
+//    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update){
+//        return bot.onWebhookUpdateReceived(update);
+//    }
 }
